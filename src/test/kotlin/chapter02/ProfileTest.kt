ee727d9c9dc6a4d29833fd46cbef88e8b9914957
boring-km <kms0644804@naver.com>
2021-08-21 22:45:36 +0900

    [chapter02] 기존 테스트와 유사한 테스트 코드 작성

--- src/test/kotlin/chapter02/ProfileTest.kt
+++ src/test/kotlin/chapter02/ProfileTest.kt
@@ -1,11 +1,12 @@
 package chapter02
 
-import org.junit.Assert.assertFalse
+import org.junit.Assert.*
 import org.junit.Test
 
 class ProfileTest {
+
     @Test
-    fun matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+    fun 반드시_맞아야_하는_기준이_맞지_않을때_정답은_false이다() {
         val profile = Profile("Bull Hockey, Inc.")
         val question = BooleanQuestion(1, "Got Bonuses?")
         val profileAnswer = Answer(question, Bool.FALSE)
@@ -13,9 +14,28 @@ class ProfileTest {
         val criteria = Criteria()
         val criteriaAnswer = Answer(question, Bool.TRUE)
         val criterion = Criterion(criteriaAnswer, Weight.MustMatch)
+
         criteria.add(criterion)
 
         val matches = profile.matches(criteria)
+
         assertFalse(matches)
+    }
+
+    @Test
+    fun 신경쓰지_않아도_되는_기준이면_정답은_true이다() {
+        val profile = Profile("Bull Hockey, Inc.")
+        val question = BooleanQuestion(1, "Got milk?")
+        val profileAnswer = Answer(question, Bool.FALSE)
+        profile.add(profileAnswer)
+        val criteria = Criteria()
+        val criteriaAnswer = Answer(question, Bool.TRUE)
+        val criterion = Criterion(criteriaAnswer, Weight.DontCare)
+
+        criteria.add(criterion)
+
+        val matches = profile.matches(criteria)
+
+        assertTrue(matches)
     }
 }
