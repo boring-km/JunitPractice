e9388c4cf808caa581e9ce0b2627ce0597cf1173
boring-km <kms0644804@naver.com>
2021-08-21 22:56:20 +0900

    [chapter02] @Before 메서드를 통해 공통코드 중복 제거 및 결론 도출

--- src/test/kotlin/chapter02/ProfileTest.kt
+++ src/test/kotlin/chapter02/ProfileTest.kt
@@ -1,21 +1,26 @@
 package chapter02
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 class ProfileTest {
 
+    private lateinit var profile: Profile
+    private lateinit var question: BooleanQuestion
+    private lateinit var criteria: Criteria
+
+    @Before
+    fun create() {
+        profile = Profile("Bull Hockey, Inc.")
+        question = BooleanQuestion(1, "Got Bonuses?")
+        criteria = Criteria()
+    }
+
     @Test
     fun 반드시_맞아야_하는_기준이_맞지_않을때_정답은_false이다() {
-        val profile = Profile("Bull Hockey, Inc.")
-        val question = BooleanQuestion(1, "Got Bonuses?")
-        val profileAnswer = Answer(question, Bool.FALSE)
-        profile.add(profileAnswer)
-        val criteria = Criteria()
-        val criteriaAnswer = Answer(question, Bool.TRUE)
-        val criterion = Criterion(criteriaAnswer, Weight.MustMatch)
-
-        criteria.add(criterion)
+        profile.add(Answer(question, Bool.FALSE))
+        criteria.add(Criterion(Answer(question, Bool.TRUE), Weight.MustMatch))
 
         val matches = profile.matches(criteria)
 
@@ -24,15 +29,8 @@ class ProfileTest {
 
     @Test
     fun 신경쓰지_않아도_되는_기준이면_정답은_true이다() {
-        val profile = Profile("Bull Hockey, Inc.")
-        val question = BooleanQuestion(1, "Got milk?")
-        val profileAnswer = Answer(question, Bool.FALSE)
-        profile.add(profileAnswer)
-        val criteria = Criteria()
-        val criteriaAnswer = Answer(question, Bool.TRUE)
-        val criterion = Criterion(criteriaAnswer, Weight.DontCare)
-
-        criteria.add(criterion)
+        profile.add(Answer(question, Bool.FALSE))
+        criteria.add(Criterion(Answer(question, Bool.TRUE), Weight.DontCare))
 
         val matches = profile.matches(criteria)
 
