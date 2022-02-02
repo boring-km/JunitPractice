78a40311136fa61c8782b62d42d361f7a1146ec9
boring-km <kms0644804@naver.com>
2022-02-02 20:59:21 +0900

    [chapter09] MatchSetTest.kt로 단위 테스트 개선

---
+++ src/test/kotlin/chapter09/MatchSetTest.kt
@@ -0,0 +1,43 @@
+package chapter09
+
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertFalse
+
+class MatchSetTest {
+
+    private lateinit var questionReimbursesTuition: BooleanQuestion
+    private lateinit var answerReimbursesTuition: Answer    // 수업을 상환한다고 대답
+    private lateinit var answerDoesNotReimburseTuition: Answer  // 수업 상환 안한다고 대답
+    private lateinit var criteria: Criteria
+    private lateinit var answers: HashMap<String, Answer>
+
+
+    @Before
+    fun createCriteria() {
+        criteria = Criteria()
+    }
+
+    @Before
+    fun createQuestionAndAnswers() {
+        questionReimbursesTuition = BooleanQuestion(1, "Reimburses tuition?")   // 정답이 true false인 질문
+        answerReimbursesTuition = Answer(questionReimbursesTuition, Bool.TRUE)  // 상환을 한다고 하면 정답
+        answerDoesNotReimburseTuition = Answer(questionReimbursesTuition, Bool.FALSE)   // 상환을 안한다고 하면 오답
+    }
+
+    private fun add(answer: Answer) {
+        answers[answer.questionText] = answer
+    }
+
+    private fun createMatchSet(): MatchSet {
+        return MatchSet(answers, criteria)
+    }
+
+    @Test
+    fun MustMatch_조건에_맞지_않는_answer이면_match_여부는_false이다() {
+        add(answerDoesNotReimburseTuition)
+        criteria.add(Criterion(answerReimbursesTuition, Weight.MustMatch))
+        assertFalse(createMatchSet().matches())
+    }
+
+}
