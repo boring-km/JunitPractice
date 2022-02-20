090c70a43a921330ea3a34f90221bf6f6c36dd3f
boring-km <kms0644804@naver.com>
2022-02-20 22:32:05 +0900

    [chapter11] 테스트 리팩토링 - 부푼 생성

--- src/test/kotlin/chapter11/util/SearchTest.kt
+++ src/test/kotlin/chapter11/util/SearchTest.kt
@@ -6,6 +6,7 @@ import org.junit.Assert.*
 import org.junit.Test
 import java.io.ByteArrayInputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 import java.util.logging.Level
 
@@ -19,14 +20,12 @@ class SearchTest {
     @Test
     @Throws(IOException::class)
     fun testSearch() {
-        val pageContent = ("There are certain queer times and occasions "
+        val stream = streamOn("There are certain queer times and occasions "
                 + "in this strange mixed affair we call life when a man "
                 + "takes this whole universe for a vast practical joke, "
                 + "though the wit thereof he but dimly discerns, and more "
                 + "than suspects that the joke is at nobody's expense but "
                 + "his own.")
-        val bytes = pageContent.toByteArray()
-        val stream = ByteArrayInputStream(bytes)
         // search
         var search = Search(stream, "practical joke", A_TITLE)
         Search.LOGGER.level = Level.OFF
@@ -37,16 +36,20 @@ class SearchTest {
             containsMatches<Match>(arrayOf(
                 Match(A_TITLE,
                     "practical joke",
-            "or a vast practical joke, though t"
+                    "or a vast practical joke, though t"
                 ))))
         stream.close()
 
         // negative
         val connection = URL("http://bit.ly/15sYPA7").openConnection()
-        val inputStream = connection.getInputStream()
+        val inputStream: InputStream = connection.getInputStream()
         search = Search(inputStream, "smelt", A_TITLE)
         search.execute()
         assertTrue(search.getMatches().isEmpty())
         stream.close()
+    }
+
+    private fun streamOn(pageContent: String): InputStream {
+        return ByteArrayInputStream(pageContent.toByteArray())
     }
 }
