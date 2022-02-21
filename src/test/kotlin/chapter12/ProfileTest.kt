44dc14673d74af44ee972e79ce13a794a77f813b
boring-km <kms0644804@naver.com>
2022-02-21 21:17:31 +0900

    [chapter12] 테스트 정리 (테스트 코드 리팩토링)

--- src/test/kotlin/chapter12/ProfileTest.kt
+++ src/test/kotlin/chapter12/ProfileTest.kt
@@ -2,14 +2,29 @@ package chapter12
 
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class ProfileTest {
+
+    private lateinit var profile: Profile
+    private lateinit var questionIsThereRelocation: BooleanQuestion
+    private lateinit var answerThereIsRelocation: Answer
+
+    @Before
+    fun createProfile() {
+        profile = Profile()
+    }
+
+    @Before
+    fun createQuestionAndAnswer() { // 이렇게 2가지를 같이 생성하는 의미로 사용해도 되려나...?
+        questionIsThereRelocation = BooleanQuestion(1, "Relocation package?")
+        answerThereIsRelocation = Answer(questionIsThereRelocation, Bool.TRUE)
+    }
+
     @Test
     fun matchesNothingWhenProfileEmpty() {
-        val profile = Profile()
-        val question = BooleanQuestion(1, "Relocation package?")
-        val criterion = Criterion(Answer(question, Bool.TRUE), Weight.DontCare)
+        val criterion = Criterion(answerThereIsRelocation, Weight.DontCare)
 
         val result = profile.matches(criterion)
 
@@ -18,11 +33,8 @@ class ProfileTest {
 
     @Test
     fun matchesWhenProfileContainsMatchingAnswer() {
-        val profile = Profile()
-        val question = BooleanQuestion(1, "Relocation package?")
-        val answer = Answer(question, Bool.TRUE)
-        profile.add(answer)
-        val criterion = Criterion(answer, Weight.Important)
+        profile.add(answerThereIsRelocation)
+        val criterion = Criterion(answerThereIsRelocation, Weight.Important)
 
         val result = profile.matches(criterion)
 
