4e8e8e423a1dbc316779090f5cbfffd2b5bf773c
boring-km <kms0644804@naver.com>
2022-02-24 22:00:08 +0900

    [chapter13] 멀티스레드 코드 테스트

--- src/main/kotlin/chapter13/ProfileMatcher.kt
+++ src/main/kotlin/chapter13/ProfileMatcher.kt
@@ -1,6 +1,7 @@
 package chapter13
 
 import java.util.concurrent.Executors
+import java.util.function.BiConsumer
 import java.util.stream.Collectors
 
 
@@ -10,19 +11,33 @@ class ProfileMatcher {
       profiles[profile.getId()] = profile
    }
 
+   val executor = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE)
+
    fun findMatchingProfiles(
-      criteria: Criteria?, listener: MatchListener
+      criteria: Criteria,
+      listener: MatchListener,
+      matchSets: MutableList<MatchSet>,
+      processFunction: BiConsumer<MatchListener, MatchSet>
    ) {
-      val executor = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE)
-      for (set in collectMatchSets(criteria)) {
-         val runnable =
-            Runnable { if (set.matches()) listener.foundMatch(profiles[set.profileId], set) }
+      for (set in matchSets) {
+         val runnable = Runnable { processFunction.accept(listener, set) }
          executor.execute(runnable)
       }
       executor.shutdown()
    }
 
-   fun collectMatchSets(criteria: Criteria?) = profiles.values.stream()
+   fun findMatchingProfiles(criteria: Criteria, listener: MatchListener) {
+      findMatchingProfiles(
+         criteria, listener, collectMatchSets(criteria), ::process
+      )
+   }
+
+   // 비동기로 실행할 코드
+   fun process(listener: MatchListener, set: MatchSet) {
+      if (set.matches()) listener.foundMatch(profiles[set.profileId], set)
+   }
+
+   fun collectMatchSets(criteria: Criteria?): MutableList<MatchSet> = profiles.values.stream()
       .map { profile: Profile ->
          profile.getMatchSet(
             criteria
--- src/test/kotlin/chapter13/ProfileMatcherTest.kt
+++ src/test/kotlin/chapter13/ProfileMatcherTest.kt
@@ -1,10 +1,14 @@
 package chapter13
 
 import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import java.util.*
+import java.util.function.BiConsumer
 import java.util.stream.Collectors
 
 
@@ -14,6 +18,7 @@ class ProfileMatcherTest {
     private lateinit var matcher: ProfileMatcher
     private lateinit var matchingProfile: Profile
     private lateinit var nonMatchingProfile: Profile
+    private lateinit var listener: MatchListener
     
     @Before
     fun create() {
@@ -39,6 +44,21 @@ class ProfileMatcherTest {
     @Before
     fun createMatcher() {
         matcher = ProfileMatcher()
+    }
+
+    @Before
+    fun createMatchListener() {
+        listener = mock(MatchListener::class.java)
+    }
+
+    @Test
+    fun processNotifiesListenerOnMatch() {
+        matcher.add(matchingProfile)
+        val set = matchingProfile.getMatchSet(criteria)
+
+        matcher.process(listener, set)
+
+        verify(listener).foundMatch(matchingProfile, set)
     }
 
     @Test
@@ -67,5 +87,38 @@ class ProfileMatcherTest {
 
     private fun nonMatchingAnswer(): Answer {
         return Answer(question, Bool.FALSE)
+    }
+
+    @Test
+    fun gathersMatchingProfiles() {
+        // 리스너가 수신하는 MatchSet 객체들의 프로파일 ID 목록을 저장할 문자열 Set 객체를 생성한다.
+        val processedSets = Collections.synchronizedSet(HashSet<String>())
+
+        // process() 메서드의 프로덕션 버전을 대신하는 함수
+        val processFunction = BiConsumer { _: MatchListener, set: MatchSet ->
+            // 각 콜백에서 MatchSet 객체의 프로파일 ID를 processedSets 변수에 추가
+            processedSets.add(set.profileId)
+        }
+
+        // 테스트용 MatchSet 객체 생성
+        val matchSets = createMatchSets(100)
+
+        // processFunction() 구현을 넘긴다.
+        // 실제로는 criteria로 테스트를 해야하지만, 내부의 로직은 matchSets의 내용을 그대로 MutableList<MatchSet>에 담아서 리턴한다.
+        matcher.findMatchingProfiles(criteria, listener, matchSets, processFunction)
+
+        // ExecutorService 객체를 가져와 모든 스레드의 실행이 완료될 때까지 기다림
+        while (!matcher.executor.isTerminated);
+
+        // processedSets 컬렉션이 테스트에서 생성된 모든 MatchSet 객체의 ID와 매칭되는지 검증한다.
+        assertThat(processedSets, equalTo(matchSets.stream().map(MatchSet::profileId).collect(Collectors.toSet())))
+    }
+
+    private fun createMatchSets(count: Int): MutableList<MatchSet> {
+        val sets = arrayListOf<MatchSet>()
+        for (i in 0 until count) {
+            sets.add(MatchSet(i.toString(), null, null))
+        }
+        return sets
     }
 }
